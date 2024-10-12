75449866b20ce9df920f0f209cb2f5444b6ff3ca
Anshika Srivastav <150316139+anshikasrivastav1000@users.noreply.github.com>
2024-10-12 23:04:39 +0530

    logical oprators

---
+++ LogicalOperator/script.js
@@ -0,0 +1,6 @@
+const userAge= 18;
+const isClgStu = ( userAge >= 18) && ( userAge <= 24);
+
+const isSclStu = ( userAge >= 5) ( userAge <= 18);
+
+const isStu = isClgStu || isClgStu
---
+++ MathObject/script.js
@@ -0,0 +1,7 @@
+const Breath = prompt('please enter B');
+const Height = prompt('please enter H');
+
+const sum = Breath * Height;
+console.log(sum);
+document.write(sum);
+
