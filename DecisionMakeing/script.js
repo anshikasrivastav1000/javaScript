251cf487fd74342845cdd993d29d3a41f8ab5753
Anshika Srivastav <150316139+anshikasrivastav1000@users.noreply.github.com>
2024-10-13 23:47:03 +0530

    ifelse statement

--- DecisionMakeing/script.js
+++ DecisionMakeing/script.js
@@ -1,12 +1,23 @@
-const username = prompt('Enter username');
-const userAge = prompt('enter user age');
+// const username = prompt('Enter username');
+// const userAge = +prompt('enter user age');
 
+// ///pareint null ka return Nan deta hai
+// //+null ka return 0 deta hai
+// if(userAge >=18 && userAge <= 24){
 
-if(userAge >=18 && userAge <= 24){
-
-    console.log(`Welcome, ${username}! You are a college student.`);
+//     console.log(`Welcome, ${username}! You are a college student.`);
     
 
-}else if(userAge >= 25){
-    console.log(`Welcome, ${username}! You are a working professional programmer`);
+// }else if(userAge >= 25){
+//     console.log(`Welcome, ${username}! You are a working professional programmer`);
+// }
+
+
+const userName = prompt('Enter username' || 'unknown');
+const userAge = parseInt(prompt('enter user age') || 'not mentioned');
+
+if( userAge >+ 18 && userAge <= 24){
+    console.log(`you're ${userName}eligible`)
+}else if(userAge >= 25 && userAge <=45){
+    console.log(`you're ${userName} working professional programmer`)
 }
