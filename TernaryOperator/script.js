8ba577779482d3d3263f70c3e6e7632d7e233a7f
Anshika Srivastav <150316139+anshikasrivastav1000@users.noreply.github.com>
2024-10-14 20:46:44 +0530

    ternary operator

---
+++ TernaryOperator/script.js
@@ -0,0 +1,7 @@
+// const userName = 5 > 2 ? "anshu" : "anshika";
+//agr condiition true hogi toh  phla wala hoga agr condition false hogi toh dusra wala hoga.
+const gender = prompt("Gender");
+
+const useMessage  = ` ${gender === 'female' ? 'female' : 'male'} is a collage student`
+
+console.log(useMessage)
