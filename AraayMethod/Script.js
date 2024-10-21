10e50ff98bcab27ad0682a90f06047dfbe8c5656
Anshika Srivastav <150316139+anshikasrivastav1000@users.noreply.github.com>
2024-10-21 22:01:53 +0530

    array method

--- AraayMethod/Script.js
+++ AraayMethod/Script.js
@@ -10,3 +10,20 @@ const unshift = arr.unshift(-1);
 const arr2 = [ "mango" , "cat" , "rat"]
 
 const add = arr.concat(arr2);
+
+const index = arr2.indexOf(cat);
+
+const isInclude = arr2.includes(cat);
+//ye boolean me answer dega isme Cat or cat ki alg value hogi agar
+
+const reverse = arr2.reverse();
+
+const shot = arr2.shot();
+//ye aplabetacial or numercical short kr deta hai or oroginal array ko bhi modify krta hai capital or cxsmaller case ko alg alg treat krta hai
+
+//slice(start, end)
+const slice = arr2.slice( ) 
+//ye add or remove kr deta hai original array ko modify kr deta hai 
+// arr2.splice(index,)
+
+const splice = arr2.splice()
