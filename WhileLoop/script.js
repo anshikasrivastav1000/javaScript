663831088454f528d65c7dc6743cea097d183f6c
Anshika Srivastav <150316139+anshikasrivastav1000@users.noreply.github.com>
2024-11-13 23:25:54 +0530

    DF

---
+++ WhileLoop/script.js
@@ -0,0 +1,14 @@
+let num = 0;
+
+while(num < 10 ){
+    console.log('hi')
+    num = num +1;
+}
+
+const name = ['akash','ash','anu','ram','sam'];
+let i = 0;
+while(i < name.length){
+    console.log(`${i + 1}. ${name[i]}` );
+    i = i + 1;
+
+}
